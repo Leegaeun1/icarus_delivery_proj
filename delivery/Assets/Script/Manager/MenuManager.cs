bab5bc933307185a802a8562e690fbc9c083055a
Leegaeun1 <igaeun013@gmail.com>
2025-11-27 20:25:49 +0900

    [feat] : 특수재료 선택 타임어택

--- delivery/Assets/Script/Manager/MenuManager.cs
+++ delivery/Assets/Script/Manager/MenuManager.cs
@@ -57,8 +57,6 @@ public class MenuManager : MonoBehaviour
             enabled = false;
             return;
         }
-        // print(instance.main_sand_cnt); // 오ㅓㅐ 안돼ㅐㅐ
-        //cardsToDisplay = 3;
 
         // --- 초기 UI 상태 설정 ---
         check_menu.SetActive(true);
--- delivery/Assets/Script/Manager/TimeManager.cs
+++ delivery/Assets/Script/Manager/TimeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -6,11 +7,18 @@ public class TimeManager : MonoBehaviour
     public TextMeshProUGUI time;
     public TextMeshProUGUI day;
     public GameManager gameManager;
+    public TextMeshProUGUI timeattack;
+    public GameObject time_UI;
+    public GameObject timeManager_UI;
 
     private int date = 1;
     private static float gameTime = 0f; // 누적 시간(초)
-    private const float timeScale = 1000f;
+    private const float timeScale = 100f;
     private const int secondsPerDay = 3 * 3600; // 하루 = 3시간
+
+    private float limitTime = 650f; // 10초 카운트 다운
+
+    private bool isclick = false;
 
     void Awake()
     {
@@ -59,7 +67,40 @@ public class TimeManager : MonoBehaviour
     void Update()
     {
         gameTime += Time.deltaTime * timeScale;
+        default_time();
+        if (isclick){
+            time_UI.SetActive(true);
+            if (limitTime > 0)
+            {
+                limitTime -= Time.deltaTime*timeScale;
 
+                // 시간이 0보다 작아지지 않게 고정
+                if (limitTime < 0) limitTime = 0;
+            }
+            else // 타임 초과시 선택 종료
+            {
+                limitTime = 0;
+                isclick = false;
+                time_UI.SetActive(true);
+                timeManager_UI.GetComponent<UIManager>().nextScene();
+            }
+
+            // 카운트다운 UI 표시
+            if (timeattack != null)
+            {
+                // 분과 초 계산
+                int min = Mathf.FloorToInt(limitTime / 60f);
+                int h = 0;
+                // "00:10" 형식으로 텍스트 갱신
+                timeattack.text = string.Format("{0:00}:{1:00}", h, min);
+            }
+        }
+        else
+            time_UI.SetActive(false);
+    }
+
+    private void default_time()
+    {
         // 날짜 계산
         int totalSeconds = Mathf.FloorToInt(gameTime / 600f) * 600; // 분 단위로 맞춤
         date = (totalSeconds / secondsPerDay) + 1;
@@ -70,9 +111,14 @@ public class TimeManager : MonoBehaviour
         int minutes = (secondsToday % 3600) / 60;
 
         // 안전하게 UI 갱신
-        if (time != null) 
+        if (time != null)
             time.text = string.Format("{0:00}:{1:00}", hours, minutes);
-        if (day != null) 
+        if (day != null)
             day.text = date.ToString() + " 일차";
+    }
+
+    public void OnBtnclick()
+    {
+        isclick = !isclick;
     }
 }
