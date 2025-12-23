d2ecf9cca22ead2cc228507d8605507b348c3331
Leegaeun1 <igaeun013@gmail.com>
2025-12-23 21:04:21 +0900

    fix : bgm부분 수정
    1. 시작시 계속 클릭소리가 나는 문제 수정(이제 클릭 시 소리 재생)
    2. 설정 UI 누르면 설정 창 뜨도록 추가

--- delivery/Assets/Script/Manager/GameManager.cs
+++ delivery/Assets/Script/Manager/GameManager.cs
@@ -7,6 +7,10 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance;
     private float startTime;
+    public GameObject setting_ui;
+    private bool isclick=false;
+
+    public AudioSource audio_source;
 
     public float ElapsedTime => Time.time - startTime; // 경과된 시간 
 
@@ -26,6 +30,22 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        //print(ElapsedTime);
+        if (Input.GetKeyUp(KeyCode.Mouse0)) // 마우스 클릭시 소리
+        {
+            audio_source.Play();
+        }
+    }
+
+    public void openUi() // 설정 열기
+    {
+        if (!isclick)
+        {
+            isclick = true;
+        }
+        else
+        {
+            isclick = false;
+        }
+        setting_ui.SetActive(isclick);
     }
 }
